41ee8fccc1e87d6a8efa3e00f529cc1b468601f9
Steven Kirk <grokys@gmail.com>
2017-07-09 17:31:43 +0200

    Add project files.
    Removed old file.

---
+++ WpfRenderTests/TestBase.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) The Avalonia Project. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ImageMagick;
+using Xunit;
+
+namespace WpfTilingTests
+{
+    public class TestBase
+    {
+        public TestBase(string outputPath)
+        {
+            string testFiles = Path.GetFullPath(@"..\..\TestFiles");
+            OutputPath = Path.Combine(testFiles, outputPath);
+        }
+
+        public string OutputPath
+        {
+            get;
+        }
+
+        protected void RenderToFile(FrameworkElement target, [CallerMemberName] string testName = "")
+        {
+            if (!Directory.Exists(OutputPath))
+            {
+                Directory.CreateDirectory(OutputPath);
+            }
+
+            string path = Path.Combine(OutputPath, testName + ".out.png");
+
+            var bitmap = new RenderTargetBitmap(
+                (int)target.Width,
+                (int)target.Height,
+                96,
+                96,
+                PixelFormats.Pbgra32);
+            Size size = new Size(target.Width, target.Height);
+            target.Measure(size);
+            target.Arrange(new Rect(size));
+            bitmap.Render(target);
+
+            var encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bitmap));
+
+            using (var s = File.OpenWrite(path))
+            {
+                encoder.Save(s);
+            }
+        }
+
+        protected void CompareImages([CallerMemberName] string testName = "")
+        {
+            string expectedPath = Path.Combine(OutputPath, testName + ".expected.png");
+            string actualPath = Path.Combine(OutputPath, testName + ".out.png");
+            using (MagickImage expected = new MagickImage(expectedPath))
+            using (MagickImage actual = new MagickImage(actualPath))
+            {
+                double error = expected.Compare(actual, ErrorMetric.RootMeanSquared);
+
+                if (error > 0.022)
+                {
+                    Assert.True(false, actualPath + ": Error = " + error);
+                }
+            }
+        }
+    }
+}
---
+++ WpfRenderTests/WpfTilingTests.cs
@@ -0,0 +1,358 @@
+﻿// Copyright (c) The Avalonia Project. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Xunit;
+
+namespace WpfTilingTests
+{
+    public class ImageBrushTests : TestBase
+    {
+        public ImageBrushTests()
+            : base(@"Media\ImageBrush")
+        {
+        }
+
+        private string BitmapPath
+        {
+            get { return System.IO.Path.Combine(OutputPath, "github_icon.png"); }
+        }
+
+        [WpfFact]
+        public void ImageBrush_NoStretch_NoTile_Alignment_TopLeft()
+        {
+            Decorator target = new Decorator
+            {
+                Width = 200,
+                Height = 200,
+                Child = new Rectangle
+                {
+                    Margin = new Thickness(8),
+                    Fill = new ImageBrush
+                    {
+                        Stretch = Stretch.None,
+                        TileMode = TileMode.None,
+                        AlignmentX = AlignmentX.Left,
+                        AlignmentY = AlignmentY.Top,
+                        ImageSource = LoadBitmap(BitmapPath),
+                    }
+                }
+            };
+
+            RenderToFile(target);
+            CompareImages();
+        }
+
+        [WpfFact]
+        public void ImageBrush_NoStretch_NoTile_Alignment_Center()
+        {
+            Decorator target = new Decorator
+            {
+                Width = 200,
+                Height = 200,
+                Child = new Rectangle
+                {
+                    Margin = new Thickness(8),
+                    Fill = new ImageBrush
+                    {
+                        Stretch = Stretch.None,
+                        TileMode = TileMode.None,
+                        AlignmentX = AlignmentX.Center,
+                        AlignmentY = AlignmentY.Center,
+                        ImageSource = LoadBitmap(BitmapPath),
+                    }
+                }
+            };
+
+            RenderToFile(target);
+            CompareImages();
+        }
+
+        [WpfFact]
+        public void ImageBrush_NoStretch_NoTile_Alignment_BottomRight()
+        {
+            Decorator target = new Decorator
+            {
+                Width = 200,
+                Height = 200,
+                Child = new Rectangle
+                {
+                    Margin = new Thickness(8),
+                    Fill = new ImageBrush
+                    {
+                        Stretch = Stretch.None,
+                        TileMode = TileMode.None,
+                        AlignmentX = AlignmentX.Right,
+                        AlignmentY = AlignmentY.Bottom,
+                        ImageSource = LoadBitmap(BitmapPath),
+                    }
+                }
+            };
+
+            RenderToFile(target);
+            CompareImages();
+        }
+
+        [WpfFact]
+        public void ImageBrush_Fill_NoTile()
+        {
+            Decorator target = new Decorator
+            {
+                Width = 920,
+                Height = 920,
+                Child = new Rectangle
+                {
+                    Margin = new Thickness(8),
+                    Fill = new ImageBrush
+                    {
+                        Stretch = Stretch.Fill,
+                        TileMode = TileMode.None,
+                        ImageSource = LoadBitmap(BitmapPath),
+                    }
+                }
+            };
+
+            RenderToFile(target);
+            CompareImages();
+        }
+
+        [WpfFact]
+        public void ImageBrush_Uniform_NoTile()
+        {
+            Decorator target = new Decorator
+            {
+                Width = 300,
+                Height = 200,
+                Child = new Rectangle
+                {
+                    Margin = new Thickness(8),
+                    Fill = new ImageBrush
+                    {
+                        Stretch = Stretch.Uniform,
+                        TileMode = TileMode.None,
+                        ImageSource = LoadBitmap(BitmapPath),
+                    }
+                }
+            };
+
+            RenderToFile(target);
+            CompareImages();
+        }
+
+        [WpfFact]
+        public void ImageBrush_UniformToFill_NoTile()
+        {
+            Decorator target = new Decorator
+            {
+                Width = 300,
+                Height = 200,
+                Child = new Rectangle
+                {
+                    Margin = new Thickness(8),
+                    Fill = new ImageBrush
+                    {
+                        Stretch = Stretch.UniformToFill,
+                        TileMode = TileMode.None,
+                        ImageSource = LoadBitmap(BitmapPath),
+                    }
+                }
+            };
+
+            RenderToFile(target);
+            CompareImages();
+        }
+
+        [WpfFact]
+        public void ImageBrush_NoStretch_NoTile_BottomRightQuarterSource()
+        {
+            Decorator target = new Decorator
+            {
+                Width = 200,
+                Height = 200,
+                Child = new Rectangle
+                {
+                    Margin = new Thickness(8),
+                    Fill = new ImageBrush
+                    {
+                        Stretch = Stretch.None,
+                        TileMode = TileMode.None,
+                        Viewport = new Rect(250, 250, 250, 250),
+                        ViewportUnits = BrushMappingMode.Absolute,
+                        ImageSource = LoadBitmap(BitmapPath),
+                    }
+                }
+            };
+
+            RenderToFile(target);
+            CompareImages();
+        }
+
+        [WpfFact]
+        public void ImageBrush_NoStretch_NoTile_BottomRightQuarterDest()
+        {
+            Decorator target = new Decorator
+            {
+                Width = 200,
+                Height = 200,
+                Child = new Rectangle
+                {
+                    Margin = new Thickness(8),
+                    Fill = new ImageBrush
+                    {
+                        Stretch = Stretch.None,
+                        TileMode = TileMode.None,
+                        Viewbox = new Rect(92, 92, 92, 92),
+                        ViewboxUnits = BrushMappingMode.Absolute,
+                        ImageSource = LoadBitmap(BitmapPath),
+                    }
+                }
+            };
+
+            RenderToFile(target);
+            CompareImages();
+        }
+
+        [WpfFact]
+        public void ImageBrush_NoStretch_NoTile_BottomRightQuarterSource_BottomRightQuarterDest()
+        {
+            Decorator target = new Decorator
+            {
+                Width = 200,
+                Height = 200,
+                Child = new Rectangle
+                {
+                    Margin = new Thickness(8),
+                    Fill = new ImageBrush
+                    {
+                        Stretch = Stretch.None,
+                        TileMode = TileMode.None,
+                        Viewbox = new Rect(0.5, 0.5, 0.5, 0.5),
+                        ViewboxUnits = BrushMappingMode.RelativeToBoundingBox,
+                        Viewport = new Rect(0.5, 0.5, 0.5, 0.5),
+                        ViewportUnits = BrushMappingMode.RelativeToBoundingBox,
+                        ImageSource = LoadBitmap(BitmapPath),
+                    }
+                }
+            };
+
+            RenderToFile(target);
+            CompareImages();
+        }
+
+        [WpfFact]
+        public void ImageBrush_NoStretch_Tile_BottomRightQuarterSource_CenterQuarterDest()
+        {
+            Decorator target = new Decorator
+            {
+                Width = 200,
+                Height = 200,
+                Child = new Rectangle
+                {
+                    Margin = new Thickness(8),
+                    Fill = new ImageBrush
+                    {
+                        Stretch = Stretch.None,
+                        TileMode = TileMode.Tile,
+                        Viewbox = new Rect(0.5, 0.5, 0.5, 0.5),
+                        ViewboxUnits = BrushMappingMode.RelativeToBoundingBox,
+                        Viewport = new Rect(0.5, 0.5, 0.5, 0.5),
+                        ViewportUnits = BrushMappingMode.RelativeToBoundingBox,
+                        ImageSource = LoadBitmap(BitmapPath),
+                    }
+                }
+            };
+
+            RenderToFile(target);
+            CompareImages();
+        }
+
+        [WpfFact]
+        public void ImageBrush_NoStretch_FlipX_TopLeftDest()
+        {
+            Decorator target = new Decorator
+            {
+                Width = 200,
+                Height = 200,
+                Child = new Rectangle
+                {
+                    Margin = new Thickness(8),
+                    Fill = new ImageBrush
+                    {
+                        Stretch = Stretch.None,
+                        TileMode = TileMode.FlipX,
+                        Viewport = new Rect(0.5, 0.5, 0.5, 0.5),
+                        ViewportUnits = BrushMappingMode.RelativeToBoundingBox,
+                        ImageSource = LoadBitmap(BitmapPath),
+                    }
+                }
+            };
+
+            RenderToFile(target);
+            CompareImages();
+        }
+
+        [WpfFact]
+        public void ImageBrush_NoStretch_FlipY_TopLeftDest()
+        {
+            Decorator target = new Decorator
+            {
+                Width = 200,
+                Height = 200,
+                Child = new Rectangle
+                {
+                    Margin = new Thickness(8),
+                    Fill = new ImageBrush
+                    {
+                        Stretch = Stretch.None,
+                        TileMode = TileMode.FlipY,
+                        Viewport = new Rect(0.5, 0.5, 0.5, 0.5),
+                        ViewportUnits = BrushMappingMode.RelativeToBoundingBox,
+                        ImageSource = LoadBitmap(BitmapPath),
+                    }
+                }
+            };
+
+            RenderToFile(target);
+            CompareImages();
+        }
+
+        [WpfFact]
+        public void ImageBrush_NoStretch_FlipXY_TopLeftDest()
+        {
+            Decorator target = new Decorator
+            {
+                Width = 200,
+                Height = 200,
+                Child = new Rectangle
+                {
+                    Margin = new Thickness(8),
+                    Fill = new ImageBrush
+                    {
+                        Stretch = Stretch.None,
+                        TileMode = TileMode.FlipXY,
+                        Viewport = new Rect(0.5, 0.5, 0.5, 0.5),
+                        ViewportUnits = BrushMappingMode.RelativeToBoundingBox,
+                        ImageSource = LoadBitmap(BitmapPath),
+                    }
+                }
+            };
+
+            RenderToFile(target);
+            CompareImages();
+        }
+
+        ImageSource LoadBitmap(string path)
+        {
+            var bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(path);
+            bitmap.EndInit();
+            return bitmap;
+        }
+    }
+}
